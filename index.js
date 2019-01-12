27d6190d8521f1a7bf330ff51e9e664ff799673d
Greg Morgan <gregm9999@gmail.com>
2019-01-12 05:07:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,4 +18,4 @@ function BoardMember(name, homeState, training){
   User.prototype.sayHi = function(){
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
   }
-}
+
